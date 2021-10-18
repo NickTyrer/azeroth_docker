87e8e2c9892bdbf07e5afac5fc8f7c0ec3bc004b
NickTyrer <nickyty8@gmail.com>
2021-10-18 09:03:37 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#CHECK_IF_DATABASE_DIR_MANGOS_IS_PRESENT (wont be if a mount is used)
+##CHECK_IF_DATABASE_DIR_MANGOS_IS_PRESENT (wont be if a mount is used)
 if [ ! -d /var/lib/mysql/mangos ]; then
 ./db_setup.sh
 fi
 
 
-#CHECK_IF_CONFIG_FILES_ARE_PRESENT (WONT_BE_IF_A_MOUNT_IS_USED)
+##CHECK_IF_CONFIG_FILES_ARE_PRESENT (WONT_BE_IF_A_MOUNT_IS_USED)
 if [ -z "$(ls -A /opt/azeroth/etc)" ]; then
 cp /opt/azeroth/mangosd.conf /opt/azeroth/realmd.conf /opt/azeroth/etc
 fi
 
 
-#SETUP_IPTABLES
+##SETUP_IPTABLES
 iptables -P INPUT DROP
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
@@ -21,7 +21,7 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
 
-#START_SERVICES
+##START_SERVICES
 if ! pgrep -x "mysqld" > /dev/null; then
 service mysql start
 fi
@@ -30,13 +30,13 @@ service ssh start
 sleep 10
 
 
-#JOIN_ZEROTIER_NETWORK_AND_WAIT_FOR_IP_TO_BE_ASSIGNED_THEN_UPDATE_DB_WITH_ASSIGNED_IP
+##JOIN_ZEROTIER_NETWORK_AND_WAIT_FOR_IP_TO_BE_ASSIGNED_THEN_UPDATE_DB_WITH_ASSIGNED_IP
 zerotier-cli join $ZT_NET
 while [ -z $(ip a | grep zt | grep inet | awk '{print $2}' | cut -d / -f 1) ] ; do sleep 10; done; ZT_IP=$(ip a | grep zt | grep inet | awk '{print $2}' | cut -d / -f 1)
 mysql -u root realmd -e "UPDATE realmlist SET address = '$ZT_IP' WHERE id = 1;"
 
 
-#SETUP_TMUX_CONSOLE
+##SETUP_TMUX_CONSOLE
 runuser -l admin -c "tmux new -d -s azeroth"
 runuser -l admin -c "tmux send-keys 'cd /opt/azeroth/bin && ./mangosd' C-m"
 runuser -l admin -c "cd /opt/azeroth/bin && ./realmd"
